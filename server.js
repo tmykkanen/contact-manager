94c7498ea61c585ad94fc1ac83faa6ee8f52be2f
tmykkanen <mr.tyler.mykkanen@gmail.com>
2018-02-13 13:43:56 -0600

    server.js setup

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const rest = require('./api');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+
+const api = new rest(app);
+
+const port = 3000;
+
+app.listen(port);
+
+console.log(`Server running on port ${port}`);
